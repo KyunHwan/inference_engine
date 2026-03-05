530362e6f05f6a74745e188e5e3165b106112597
KyunHwan <kyunhwan.peter@gmail.com>
2026-03-05 18:22:55 +0900

    modified start_ray.sh

--- start_ray.sh
+++ start_ray.sh
@@ -5,10 +5,10 @@ HEAD_IP="192.168.0.134"
 HOSTNAME=$(hostname)
 
 case "$HOSTNAME" in
-  robros-MS-7E59)
+  robros-ai1)
     ray start --head --port=6379
     ;;
-  robros-ai1)
+  robros-MS-7E59)
     ray start --address=${HEAD_IP}:6379 \
       --resources='{"inference_pc": 1}'
     ;;
