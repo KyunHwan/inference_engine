a4d6c6153514594240bb25923a67e33227397d4e
KyunHwan <kyunhwan.peter@gmail.com>
2026-03-05 18:15:04 +0900

    changed writability of start_ray.sh

--- start_ray.sh
+++ start_ray.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # start_ray.sh — run on each machine
 
-HEAD_IP="192.168.0.114"
+HEAD_IP="192.168.0.134"
 HOSTNAME=$(hostname)
 
 case "$HOSTNAME" in
   robros-MS-7E59)
     ray start --head --port=6379
     ;;
-  robros-5090)
+  robros-ai1)
     ray start --address=${HEAD_IP}:6379 \
       --resources='{"inference_pc": 1}'
     ;;
